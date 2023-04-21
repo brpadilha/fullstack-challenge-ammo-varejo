2dc2bb41e9c686fda996e2274ec89ed1024637f9
Bruno <brpadilha.dev@gmail.com>
2023-04-21 15:43:18 -0300

    cria teste para o product service

--- backend-ammo-varejo/src/products/product.service.spec.ts
+++ backend-ammo-varejo/src/products/product.service.spec.ts
@@ -1,4 +1,3 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { ProductService } from './product.service';
 import { ProductGatewayInMemory } from './gateways/product-gateway-in-memory';
 
