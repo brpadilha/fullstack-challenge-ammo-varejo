0a52528d35f664b483bc22a5b7a2fc31c621c8aa
Bruno <brpadilha.dev@gmail.com>
2023-04-21 14:57:28 -0300

    cria mock

---
+++ frontend-ammo-varejo/src/helper/mock.ts
@@ -0,0 +1,14 @@
+export const productMock = [
+            {
+              id: 1,
+              name: 'Lençol avulso mock',
+              description: 'Lençol avulso mock description',
+              promotionPrice: 0,
+              price: 0,
+              images: [
+                {
+                  id: 1,
+                  url: 'https://www.ammo.com.br/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/1/0/1000x1000_1_1_1.jpg',
+                }],
+            },
+          ]
